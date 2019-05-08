de76fa2230b9a4c2953975a7194603215a863630
Filipe Santos <filipe.santos@goin.com.br>
2019-05-08 14:31:36 -0300

    ajuste import detalhescontratantes

--- src/test/java/br/com/testfilipe/test/pageobject/salesforce/Detalhes_Contratantes.java
+++ src/test/java/br/com/testfilipe/test/pageobject/salesforce/Detalhes_Contratantes.java
@@ -9,14 +9,18 @@ import org.openqa.selenium.WebDriver;
  */
 
 public class Detalhes_Contratantes extends BaseWebPage {
-    private static By QUANTITYPARCEL_TEXTBOX = By.xpath("//div[@id='00N0j000000jbgQ_ileinner']");
+    public Detalhes_Contratantes(WebDriver webDriver) {
+		super(webDriver);
+		// TODO Auto-generated constructor stub
+	}
 
-    public Detalhes_Contratantes (WebDriver webDriver)
-        super WebDriver
-    // TODO Auto-generated constructor stub
+	private static By QUANTITYPARCEL_TEXTBOX = By.xpath("//div[@id='00N0j000000jbgQ_ileinner']");
 
-    @Override
-    public boolean isDisplayed() {
-        return false;
-    }
+	@Override
+	public boolean isDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+  
 }
