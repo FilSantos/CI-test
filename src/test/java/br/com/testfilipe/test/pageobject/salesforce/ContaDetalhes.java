371b33299fedcfa525821d91d5600cac636e9d7d
Santos <738994@cognizant.com>
2019-05-09 11:41:08 -0300

    Métodos Everton

--- src/test/java/br/com/testfilipe/test/pageobject/salesforce/ContaDetalhes.java
+++ src/test/java/br/com/testfilipe/test/pageobject/salesforce/ContaDetalhes.java
@@ -4,12 +4,13 @@ import br.com.testfilipe.core.selenium.pageobject.BaseWebPage;
 import com.sun.corba.se.impl.encoding.CodeSetConversion;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ContaDetalhes extends BaseWebPage {
 
     private static By ACCOUNTNAME_TEXTBOX = By.xpath("//td[contains(text(),'Nome da conta')]/following-sibling::td[1]/div");
     private static By SEX_TEXTBOX = By.xpath("//td[contains(text(),'Sexo')]/following-sibling::td[1]/div");
-    private static By DATEOFBIRD_TEXBOX = By.xpath("//td[contains(text(),'Data de nascimento')]/following-sibling::td[1]/div");
+    private static By DATEOFBIRTH_TEXBOX = By.xpath("//td[contains(text(),'Data de nascimento')]/following-sibling::td[1]/div");
     private static By CPF_TEXBOX = By.xpath("//td[contains(text(),'CPF')]/following-sibling::td[1]/div");
     private static By MARITALSTATUS_TEXBOX = By.xpath("//td[contains(text(),'Estado civil')]/following-sibling::td[1]/div");
     private static By CELLPHONENUM_TEXTBOX = By.xpath("//td[contains(text(),'Celular')]/following-sibling::td[1]/div");
@@ -26,4 +27,113 @@ public class ContaDetalhes extends BaseWebPage {
     public boolean isDisplayed() {
         return false;
     }
+
+    /**Retorna o nome do contratante
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getAccountName () throws Exception{
+
+        WebElement text = searchElement.findElementBy (ACCOUNTNAME_TEXTBOX "Nome da conta");
+
+        return text.getText()
+    }
+
+    /** Retorna o sexo do contratante
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getSex () throws Exception{
+
+        WebElement text = searchElement.findElementBy (SEX_TEXTBOX "sexo");
+
+        return text.getText()
+    }
+
+    /**Retorna a data de nascimento
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getDateOfBirth () throws Exception{
+
+        WebElement text = searchElement.findElementBy(DATEOFBIRTH_TEXBOX "Data de nascimento");
+
+        return text.getText()
+    }
+
+    /**Retorna o cpf
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getCPF () throws Exception{
+
+        WebElement text = searchElement.findElementBy(CPF_TEXBOX "CPF");
+
+        return text.getText()
+    }
+
+    /**Retorna o estado civil
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getMaritalStatus () throws Exception{
+
+        WebElement text = searchElement.findElementBy(MARITALSTATUS_TEXBOX "Estado civil");
+
+        return text.getText()
+    }
+
+    /**Retorna número do celular
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getCellphoneNumber () throws Exception{
+
+        WebElement text = searchElement.findElementBy(CELLPHONENUM_TEXTBOX "Número do celular");
+
+        return text.getText()
+    }
+
+    /**Retorna o telefone comercial
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getCommercialNumber () throws Exception{
+
+        WebElement text = searchElement.findElementBy(COMMERCPHONENUM_TEXTBOX "Telefone Comercial");
+
+        return text.getText()
+    }
+
+    /**Retorna o telefone residencial
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    private String getPhoneNumber () throws Exception{
+
+        WebElement text = searchElement.findElementBy(PHONENUMBER_TEXTBOX "Telefone residencial")
+
+        return text.getText()
+    }
+
+    /**Clica em Editar (aba Endereços)
+     * @author everton cognizant
+     * @throws Exception
+     */
+    private void tapEditAdress () throws Exception{
+
+        WebElement link = searchElement.findElementBy(EDITADRESS_LINK "Endereços - EDITAR");
+
+        command.click(link);
+    }
+
+
 }
--- src/test/java/br/com/testfilipe/test/pageobject/salesforce/ContaSeguradoEndereco.java
+++ src/test/java/br/com/testfilipe/test/pageobject/salesforce/ContaSeguradoEndereco.java
@@ -4,6 +4,7 @@ import br.com.testfilipe.core.selenium.pageobject.BaseWebPage;
 import com.fasterxml.jackson.databind.ser.Serializers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * @author everton cognizant
@@ -31,6 +32,90 @@ public class ContaSeguradoEndereco extends BaseWebPage {
     public boolean isDisplayed() {
         // TODO Auto-generated method stub
         return false;
+    }
+
+    /**Retorna o CEP
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    public String getCEP () throws Exception{
+
+        WebElement text = searchElement.findElementBy(CEP_TEXTBOX "CEP");
+
+        return text.getText()
+    }
+
+    /**Retorna o logradouro
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    public String getStreet () throws Exception{
+
+        WebElement text = searchElement.findElementBy(STREET_TEXTAREA "Logradouro");
+
+        return text.getText()
+    }
+
+    /**Retorna o complemento
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    public String getComplement () throws Exception{
+
+        WebElement text = searchElement.findElementBy(COMPLEMENT_TEXTBOX "Complemento");
+
+        return text.getText()
+    }
+
+    /**Retorna o número da residencia
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    public String getResidencialNumber () throws Exception{
+
+        WebElement text = searchElement.findElementBy(RESIDENCIALNUMBER_TEXTBOX "Número da residencia");
+
+        return text.getText()
+    }
+
+    /**Retorna o bairro
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    public String getDistrict () throws Exception{
+
+        WebElement text = searchElement.findElementBy(DISTRICT_TEXTBOX "Bairro");
+
+        return text.getText()
+    }
+
+    /**Retorna a cidade
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    public String getCity () throws Exception{
+
+        WebElement text = searchElement.findElementBy(CITY_TEXTBOX "Cidade");
+
+        return text.getText()
+    }
+
+    /**Retorna o estado
+     * @author everton cognizant
+     * @return String
+     * @throws Exception
+     */
+    public String getState () throws Exception{
+
+        WebElement text = searchElement.findElementBy(STATE_SELECT "Estado");
+
+        return text.getText()
     }
 
 }
